342a79b64c91ebd5a49adc09fa53c6477e974967
nguyenthithuy <ntthuyyl@gmail.com>
2018-11-08 17:31:56 +0700

    deploy in local

---
+++ test.py
@@ -0,0 +1,30 @@
+import os
+import base64
+from io import BytesIO
+
+from keras.preprocessing import image
+from flask import Flask, make_response, request, render_template
+from werkzeug.exceptions import BadRequest
+from serving import (
+    load_model,
+    evaluate_input,
+)
+from support import (
+	show_img,
+	read_img
+)
+
+CWD_PATH = os.getcwd()
+
+load_model()
+def colorsize(image_name):
+	PATH_TO_IMAGE = os.path.join(CWD_PATH, "images", image_name)
+	img = evaluate_input(PATH_TO_IMAGE)
+	show_img(image.img_to_array(img))
+
+while (True):  
+	# image_name format is .jpg .png
+    inp = input('image_name = ')
+    if inp == '':
+        break
+    colorsize(inp)
